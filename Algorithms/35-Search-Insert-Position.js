1381fd4b40a9245b89bfb3545feb1ae4d1389427
macxu <adherentxu@gmail.com>
2018-02-01 22:14:48 +0800

    二分查找

--- Algorithms/35-Search-Insert-Position.js
+++ Algorithms/35-Search-Insert-Position.js
@@ -2,16 +2,20 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
- * Date: 2018-02-01 
+ * Date: 2018-02-01
+ * 算法： 二分查找
  */
 var searchInsert = function(nums, target) {
-  let mid, head = 0, tail = nums.length - 1
-  while (head <= tail) {
-      mid = (head + tail) >> 1
-  
-      if (nums[mid] === target) return mid
-      if (nums[mid] < target) head = mid + 1
-      if (nums[mid] > target) tail = mid - 1
+
+  var low = 0;
+  var high = nums.length - 1;
+
+  while (low <= high) {
+    var mid = (low + high)/2 >> 0;
+    var guess = nums[mid];
+    if(guess === target) return mid;
+    if(guess > target)  high = mid - 1
+    else  low = mid + 1
   }
-  return head
+  return low
 };
