a9572cdc8535bf367d983b2837da97f5a4c86d36
徐梓昊 <adherentxu@gmail.com>
2021-04-05 21:31:31 +0800

    feat: 增加第700题

---
+++ Algorithms/700-二叉搜索树中的搜索.ts
@@ -0,0 +1,5 @@
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+  if (root == null || val == root.val) return root;
+  return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val)
+  
+};
