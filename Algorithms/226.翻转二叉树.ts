7efedcc8a9ec77b656b735c707d7b7fbfaed32b1
徐梓昊 <adherentxu@gmail.com>
2021-04-06 20:30:44 +0800

    feat: 增加226

---
+++ Algorithms/226.翻转二叉树.ts
@@ -0,0 +1,8 @@
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if(!root) return null;
+  const left = invertTree(root.left);
+  const right = invertTree(root.right);
+  root.left = right;
+  root.right = left;
+  return root;
+};
--- Algorithms/700-二叉搜索树中的搜索.ts
+++ Algorithms/700-二叉搜索树中的搜索.ts
@@ -1,5 +1,4 @@
 function searchBST(root: TreeNode | null, val: number): TreeNode | null {
   if (root == null || val == root.val) return root;
   return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val)
-  
 };
