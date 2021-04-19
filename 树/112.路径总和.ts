a140cb4590099cd7505a74acaf77995a260e2225
徐梓昊 <adherentxu@gmail.com>
2021-04-19 21:53:27 +0800

    feat: 增加112

---
+++ 树/112.路径总和.ts
@@ -0,0 +1,5 @@
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  if (!root) return false;
+  if (!root.left && !root.right) return root.val == targetSum;
+  return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+};
